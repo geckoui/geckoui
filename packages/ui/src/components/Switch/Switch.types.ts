5442a45f2742dac61af91f19e0d88c6494575bfa
Wai Yan Phyo <oyhpnayiaw@gmail.com>
2026-04-15 17:47:37 +0700

    Rewrite switch component with native input element

--- packages/ui/src/components/Switch/Switch.types.ts
+++ packages/ui/src/components/Switch/Switch.types.ts
@@ -1,23 +1,11 @@
-import type { SwitchProps as HeadlessSwitchProps } from "@headlessui/react";
-
-/** Extensible size map - To allow module augmentation */
 export interface SwitchSizeMap {
   sm: unknown;
   md: unknown;
 }
 
-export interface SwitchProps extends Omit<HeadlessSwitchProps, "className"> {
-  /**
-   * The size of the switch.(default: 'md')
-   * */
+export interface SwitchProps
+  extends Omit<React.InputHTMLAttributes<HTMLInputElement>, "onChange" | "size" | "type"> {
+  onChange?: (checked: boolean) => void;
   size?: keyof SwitchSizeMap;
-
-  /**
-   * Class name for the switch.
-   * */
-  className?: string;
-  /**
-   * Style for the thumb of the switch.
-   * */
   thumbClassName?: string;
 }
